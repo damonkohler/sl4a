cc8eec880bb49278797c633cdf566a52ac9fc20d
Damon Kohler <damonkohler@gmail.com>
2010-04-05 22:05:09 +0200

    Fix broken buildJsonList method and add test for it. Undo workarounds added to Python scripts say_weather and notify_weather.

---
+++ android/AndroidScriptingEnvironment/test/com/google/ase/jsonrpc/JsonResultBuildersTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright (C) 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.google.ase.jsonrpc;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import junit.framework.TestCase;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class JsonResultBuildersTest extends TestCase {
+
+  public void testBuildJsonList() throws JSONException {
+    List<JSONObject> objects = new ArrayList<JSONObject>();
+    JSONObject foo = new JSONObject();
+    JSONObject bar = new JSONObject();
+    JSONObject baz = new JSONObject();
+    objects.add(foo);
+    objects.add(bar);
+    objects.add(baz);
+    JSONArray result = JsonResultBuilders.buildJsonList(objects);
+    assertEquals(result.get(0), foo);
+    assertEquals(result.get(1), bar);
+    assertEquals(result.get(2), baz);
+  }
+}
