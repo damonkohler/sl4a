3aa75b4f773c5a3f0f7fcd8a9deeb6a544ca3bf3
Damon Kohler <damonkohler@gmail.com>
2010-04-29 21:31:16 +0200

    Pushed down Server into JsonRpcServer since it became an unneccessary abstraction.

--- android/AndroidScriptingEnvironment/src/com/google/ase/jsonrpc/JsonRpcServer.java
+++ android/AndroidScriptingEnvironment/src/com/google/ase/jsonrpc/JsonRpcServer.java
@@ -18,18 +18,28 @@ package com.google.ase.jsonrpc;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.NetworkInterface;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.google.ase.AseLog;
-import com.google.ase.Server;
 import com.google.ase.rpc.MethodDescriptor;
 import com.google.ase.rpc.RpcError;
 
@@ -38,7 +48,7 @@ import com.google.ase.rpc.RpcError;
  * 
  * @author Damon Kohler (damonkohler@gmail.com)
  */
-public class JsonRpcServer extends Server {
+public class JsonRpcServer {
 
   /**
    * A map of strings to known RPCs.
@@ -50,6 +60,10 @@ public class JsonRpcServer extends Server {
    */
   private final List<RpcReceiver> mReceivers = new ArrayList<RpcReceiver>();
 
+  private ServerSocket mServer;
+  private Thread mServerThread;
+  private final CopyOnWriteArraySet<Thread> mNetworkThreads;
+
   /**
    * Construct a {@link JsonRpcServer} connected to the provided {@link RpcReceiver}s.
    * 
@@ -57,6 +71,7 @@ public class JsonRpcServer extends Server {
    *          the {@link RpcReceiver}s to register with the server
    */
   public JsonRpcServer(List<RpcReceiver> receivers) {
+    mNetworkThreads = new CopyOnWriteArraySet<Thread>();
     for (RpcReceiver receiver : receivers) {
       registerRpcReceiver(receiver);
     }
@@ -81,17 +96,97 @@ public class JsonRpcServer extends Server {
     mReceivers.add(receiver);
   }
 
-  @Override
-  public void shutdown() {
-    super.shutdown();
-    // Notify all RPC receiving objects. They may have to clean up some of their state.
-    for (RpcReceiver receiver : mReceivers) {
-      receiver.shutdown();
+  private InetAddress getPublicInetAddress() throws UnknownHostException, SocketException {
+    Enumeration<NetworkInterface> nets = NetworkInterface.getNetworkInterfaces();
+    for (NetworkInterface netint : Collections.list(nets)) {
+      Enumeration<InetAddress> addresses = netint.getInetAddresses();
+      for (InetAddress address : Collections.list(addresses)) {
+        if (!address.getHostAddress().equals("127.0.0.1")) {
+          return address;
+        }
+      }
     }
+    return InetAddress.getLocalHost();
   }
 
-  @Override
-  protected void process(BufferedReader in, PrintWriter out) throws JSONException, IOException {
+  /**
+   * Starts the RPC server bound to the localhost address.
+   * 
+   * @return the port that was allocated by the OS
+   */
+  public InetSocketAddress startLocal() {
+    InetAddress address;
+    try {
+      address = InetAddress.getLocalHost();
+      mServer = new ServerSocket(0 /* port */, 5 /* backlog */, address);
+    } catch (Exception e) {
+      AseLog.e("Failed to start server.", e);
+      return null;
+    }
+    int port = start(address);
+    return new InetSocketAddress(address, port);
+  }
+
+  /**
+   * Starts the RPC server bound to the public facing address.
+   * 
+   * @return the port that was allocated by the OS
+   */
+  public InetSocketAddress startPublic() {
+    InetAddress address;
+    try {
+      address = getPublicInetAddress();
+      mServer = new ServerSocket(0 /* port */, 5 /* backlog */, address);
+    } catch (Exception e) {
+      AseLog.e("Failed to start server.", e);
+      return null;
+    }
+    int port = start(address);
+    return new InetSocketAddress(address, port);
+  }
+
+  private int start(InetAddress address) {
+    mServerThread = new Thread() {
+      @Override
+      public void run() {
+        while (true) {
+          try {
+            Socket sock = mServer.accept();
+            startConnectionThread(sock);
+          } catch (IOException e) {
+            AseLog.e("Failed to accept connection.", e);
+          }
+        }
+      }
+    };
+    mServerThread.start();
+    AseLog.v("Bound to " + address.getHostAddress() + ":" + mServer.getLocalPort());
+    return mServer.getLocalPort();
+  }
+
+  private void startConnectionThread(final Socket sock) {
+    final Thread networkThread = new Thread() {
+      @Override
+      public void run() {
+        AseLog.v("Server thread " + getId() + " started.");
+        try {
+          BufferedReader in = new BufferedReader(new InputStreamReader(sock.getInputStream()));
+          PrintWriter out = new PrintWriter(sock.getOutputStream(), true);
+          process(in, out);
+        } catch (Exception e) {
+          AseLog.e("Server error.", e);
+        } finally {
+          mNetworkThreads.remove(this);
+          AseLog.v("Server thread " + getId() + " died.");
+        }
+      }
+    };
+
+    mNetworkThreads.add(networkThread);
+    networkThread.start();
+  }
+
+  private void process(BufferedReader in, PrintWriter out) throws JSONException, IOException {
     String data;
     while ((data = in.readLine()) != null) {
       AseLog.v("Received: " + data);
@@ -123,5 +218,23 @@ public class JsonRpcServer extends Server {
       result = rpcInfo.invoke(params);
     }
     return result;
+  }
+
+  public void shutdown() {
+    // Interrupt the server thread to ensure that beyond this point there are
+    // no incoming requests.
+    mServerThread.interrupt();
+    // Since the server thread is not running, the mNetworkThreads set can only
+    // shrink from this point onward. We can just cancel all of the running
+    // threads. In the worst case, one of the running threads will already have
+    // shut down. Since this is a CopyOnWriteSet, we don't have to worry about
+    // concurrency issues while iterating over the set of threads.
+    for (Thread networkThread : mNetworkThreads) {
+      networkThread.interrupt();
+    }
+    // Notify all RPC receiving objects. They may have to clean up some of their state.
+    for (RpcReceiver receiver : mReceivers) {
+      receiver.shutdown();
+    }
   }
 }
