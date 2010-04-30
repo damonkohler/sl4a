cf4048012475da04319fda81f9676d709c695746
Damon Kohler <damonkohler@gmail.com>
2010-04-30 18:23:52 +0200

    Add ActivityManagerFacade to the facade configuration.

--- android/AndroidScriptingEnvironment/src/com/google/ase/facade/FacadeConfiguration.java
+++ android/AndroidScriptingEnvironment/src/com/google/ase/facade/FacadeConfiguration.java
@@ -61,6 +61,7 @@ public class FacadeConfiguration {
     list.addAll(MethodDescriptor.collectFrom(CameraFacade.class));
     list.addAll(MethodDescriptor.collectFrom(PowerManagerFacade.class));
     list.addAll(MethodDescriptor.collectFrom(WifiFacade.class));
+    list.addAll(MethodDescriptor.collectFrom(ActivityManagerFacade.class));
 
     // Bluetooth is not available before Android 2.0.
     try {
@@ -126,6 +127,7 @@ public class FacadeConfiguration {
     receivers.add(new CameraFacade());
     receivers.add(new PowerManagerFacade(service));
     receivers.add(new WifiFacade(service));
+    receivers.add(new ActivityManagerFacade(service, androidFacade));
 
     // Bluetooth is not available before Android 2.0.
     try {
