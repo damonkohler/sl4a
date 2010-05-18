466ce841eb6625ca9cf9eaf3669d1c09d822d9a5
Damon Kohler <damonkohler@gmail.com>
2010-05-19 01:05:58 +0200

    Extracted a few methods.

--- android/PythonForAndroid/src/com/googlecode/pythonforandroid/Main.java
+++ android/PythonForAndroid/src/com/googlecode/pythonforandroid/Main.java
@@ -17,31 +17,44 @@ public class Main extends Activity {
     Intent intent = getIntent();
     if (intent.getAction().equals(Constants.ACTION_DISCOVER_INTERPRETERS)) {
       PythonInterpreter interpreter = new PythonInterpreter();
-      Intent result = new Intent();
-      Bundle description = new Bundle();
-      description.putString("name", interpreter.getName());
-      description.putString("niceName", interpreter.getNiceName());
-      description.putString("extension", interpreter.getExtension());
-      description.putString("binaryAbsolutePath", interpreter.getBinary().getAbsolutePath());
-      description.putBoolean("hasInterpreterArchive", interpreter.hasInterpreterArchive());
-      description.putBoolean("hasInterpreterExtrasArchive", interpreter
-          .hasInterpreterExtrasArchive());
-      description.putBoolean("hasScriptsArchive", interpreter.hasScriptsArchive());
-      result.putExtra("description", description);
-      Bundle environment = new Bundle();
-      environment.putString("PYTHONHOME", PythonInterpreterProcess.PYTHON_HOME);
-      environment.putString("PYTHONPATH", PythonInterpreterProcess.PYTHON_EXTRAS + ":"
-          + Constants.SCRIPTS_ROOT);
-      File tmp = new File(PythonInterpreterProcess.PYTHON_EXTRAS + "tmp/");
-      if (!tmp.isDirectory()) {
-        tmp.mkdir();
-      }
-      environment.putString("TEMP", tmp.getAbsolutePath());
-      result.putExtra("environment", environment);
-      setResult(RESULT_OK, result);
+      setResult(RESULT_OK, buildResultIntent(interpreter));
       finish();
       return;
     }
     setContentView(R.layout.main);
+  }
+
+  private Intent buildResultIntent(PythonInterpreter interpreter) {
+    Intent result = new Intent();
+    Bundle description = buildDescriptionBundle(interpreter);
+    result.putExtra("description", description);
+    Bundle environment = new Bundle();
+    buildEnvironmentBundle(environment);
+    result.putExtra("environment", environment);
+    return result;
+  }
+
+  private void buildEnvironmentBundle(Bundle environment) {
+    environment.putString("PYTHONHOME", PythonInterpreterProcess.PYTHON_HOME);
+    environment.putString("PYTHONPATH", PythonInterpreterProcess.PYTHON_EXTRAS + ":"
+        + Constants.SCRIPTS_ROOT);
+    File tmp = new File(PythonInterpreterProcess.PYTHON_EXTRAS + "tmp/");
+    if (!tmp.isDirectory()) {
+      tmp.mkdir();
+    }
+    environment.putString("TEMP", tmp.getAbsolutePath());
+  }
+
+  private Bundle buildDescriptionBundle(PythonInterpreter interpreter) {
+    Bundle description = new Bundle();
+    description.putString("name", interpreter.getName());
+    description.putString("niceName", interpreter.getNiceName());
+    description.putString("extension", interpreter.getExtension());
+    description.putString("binaryAbsolutePath", interpreter.getBinary().getAbsolutePath());
+    description.putBoolean("hasInterpreterArchive", interpreter.hasInterpreterArchive());
+    description.putBoolean("hasInterpreterExtrasArchive", interpreter
+        .hasInterpreterExtrasArchive());
+    description.putBoolean("hasScriptsArchive", interpreter.hasScriptsArchive());
+    return description;
   }
 }
