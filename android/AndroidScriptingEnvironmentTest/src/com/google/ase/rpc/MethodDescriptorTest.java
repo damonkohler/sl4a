1be7695ae4374662abe3745890b6ef161b70e200
Damon Kohler <damonkohler@gmail.com>
2010-07-02 23:30:52 +0200

    Fix tests.

--- android/AndroidScriptingEnvironmentTest/src/com/google/ase/rpc/MethodDescriptorTest.java
+++ android/AndroidScriptingEnvironmentTest/src/com/google/ase/rpc/MethodDescriptorTest.java
@@ -24,6 +24,11 @@ import java.util.List;
 
 import junit.framework.TestCase;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.google.ase.jsonrpc.RpcReceiver;
+
 /**
  * Tests {@link MethodDescriptor}.
  * 
@@ -33,12 +38,20 @@ public class MethodDescriptorTest extends TestCase {
 
   public void testCollectFromClass() {
     @SuppressWarnings("unused")
-    class A {
+    class A extends RpcReceiver {
+      public A() {
+        super(null);
+      }
+
       public void notRpc() {
       }
 
       @Rpc(description = "rpc", returns = "nothing")
       public void rpc() {
+      }
+
+      @Override
+      public void shutdown() {
       }
     }
 
@@ -138,5 +151,31 @@ public class MethodDescriptorTest extends TestCase {
       }
       return result;
     }
+  }
+
+  public void testConvertParameter() throws JSONException, RpcError {
+    Class<?>[] types =
+        new Class[] { Object.class, Boolean.class, Boolean.class, Boolean.class, Integer.class,
+          Double.class, String.class };
+    JSONArray parameters = new JSONArray();
+    parameters.put(null);
+    parameters.put(true);
+    parameters.put(0);
+    parameters.put(1);
+    parameters.put(42);
+    parameters.put(3.14159);
+    parameters.put("Hello, world!");
+    // TODO(damonkohler): Test converting JSONArrays into Lists.
+    Object[] args = new Object[parameters.length()];
+    for (int i = 0; i < parameters.length(); i++) {
+      args[i] = MethodDescriptor.convertParameter(parameters, i, types[i]);
+    }
+    assertNull(args[0]);
+    assertEquals(Boolean.TRUE, args[1]);
+    assertEquals(Boolean.FALSE, args[2]);
+    assertEquals(Boolean.TRUE, args[3]);
+    assertEquals(42, args[4]);
+    assertEquals(3.14159, args[5]);
+    assertEquals("Hello, world!", args[6]);
   }
 }
