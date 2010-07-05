60afd24e42c347e46793b16d6c5c8f96412e59a7
Alexey Reznichenko <alexey.reznichenko@gmail.com>
2010-07-05 16:47:27 +0200

    Added ScriptForAndroidTemplate project.

---
+++ android/ScriptForAndroidTemplate/jni/Application.mk
@@ -0,0 +1,2 @@
+APP_PROJECT_PATH := /home/damonkohler/ase_src/android/AndroidScriptingEnvironment
+APP_MODULES      := com_google_ase_Exec
---
+++ android/ScriptForAndroidTemplate/src/com/googlecode/script/ScriptService.java
@@ -0,0 +1,70 @@
+package com.googlecode.script;
+
+import android.app.Service;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Binder;
+import android.os.IBinder;
+
+import com.google.ase.AndroidProxy;
+import com.google.ase.AseLog;
+import com.google.ase.ScriptLauncher;
+import com.google.ase.ScriptStorageAdapter;
+import com.google.ase.exception.AseException;
+import com.google.ase.interpreter.InterpreterAgent;
+import com.google.ase.interpreter.InterpreterConfiguration;
+
+import java.io.File;
+
+public class ScriptService extends Service {
+  private final IBinder mBinder;
+
+  public class LocalBinder extends Binder {
+    public ScriptService getService() {
+      return ScriptService.this;
+    }
+  }
+
+  public ScriptService() {
+    mBinder = new LocalBinder();
+  }
+
+
+  @Override
+  public void onStart(Intent intent, int startId) {
+    super.onStart(intent, startId);
+    ScriptApplication app = (ScriptApplication) getApplication();
+    InterpreterConfiguration config = app.getInterpreterConfiguration();
+    Resources resources = getResources();
+    int id = R.raw.hello_world;
+    String name = resources.getText(id).toString();
+    String fileName = name.substring(name.lastIndexOf('/') + 1, name.length());
+
+    InterpreterAgent interpreter = config.getInterpreterForScript(fileName);
+
+    if (interpreter == null || !interpreter.isInstalled(this)) {
+      AseLog.e("Cannot find an interpreter for script " + fileName);
+      stopSelf(startId);
+    }
+
+    // Copies script to disk(sdcard).
+    File script = ScriptStorageAdapter.copyFromStream(fileName, resources.openRawResource(id));
+
+    AndroidProxy proxy = new AndroidProxy(this, null);
+    proxy.startLocal();
+    ScriptLauncher launcher = new ScriptLauncher(script, proxy.getAddress(), config);
+    try {
+      launcher.launch();
+    } catch (AseException e) {
+      AseLog.e(e);
+      proxy.shutdown();
+      stopSelf(startId);
+    }
+  }
+
+  @Override
+  public IBinder onBind(Intent intent) {
+    return mBinder;
+  }
+
+}
