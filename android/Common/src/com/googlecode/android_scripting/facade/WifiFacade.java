52b9a311c4a28c539454abd2860b25c0fa51c5e9
Damon Kohler <damonkohler@gmail.com>
2010-08-05 16:37:00 +0200

    Add wifi lock support.

--- android/Common/src/com/googlecode/android_scripting/facade/WifiFacade.java
+++ android/Common/src/com/googlecode/android_scripting/facade/WifiFacade.java
@@ -6,6 +6,7 @@ import android.app.Service;
 import android.content.Context;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.net.wifi.WifiManager.WifiLock;
 
 import com.googlecode.android_scripting.jsonrpc.RpcReceiver;
 import com.googlecode.android_scripting.rpc.Rpc;
@@ -16,16 +17,39 @@ public class WifiFacade extends RpcReceiver {
 
   private final Service mService;
   private final WifiManager mWifi;
+  private WifiLock mLock;
 
   public WifiFacade(FacadeManager manager) {
     super(manager);
     mService = manager.getService();
     mWifi = (WifiManager) mService.getSystemService(Context.WIFI_SERVICE);
+    mLock = null;
   }
 
   @Rpc(description = "Returns the list of access points found during the most recent Wifi scan.")
   public List<ScanResult> wifiGetScanResults() {
     return mWifi.getScanResults();
+  }
+
+  @Rpc(description = "Acquires a full Wifi lock.")
+  public void wifiLockAcquireFull() {
+    if (mLock == null) {
+      mLock = mWifi.createWifiLock(WifiManager.WIFI_MODE_FULL, "sl4a");
+    }
+  }
+
+  @Rpc(description = "Acquires a scan only Wifi lock.")
+  public void wifiLockAcquireScanOnly() {
+    if (mLock == null) {
+      mLock = mWifi.createWifiLock(WifiManager.WIFI_MODE_SCAN_ONLY, "sl4a");
+    }
+  }
+
+  @Rpc(description = "Releases a previously acquired Wifi lock.")
+  public void wifiLockRelease() {
+    if (mLock != null) {
+      mLock.release();
+    }
   }
 
   @Rpc(description = "Starts a scan for Wifi access points.", returns = "True if the scan was initiated successfully.")
@@ -50,5 +74,6 @@ public class WifiFacade extends RpcReceiver {
 
   @Override
   public void shutdown() {
+    wifiLockRelease();
   }
 }
