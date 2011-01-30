9db220be79313611b48fe6065449ef4bde12dfbf
rjmatthews62 <rjmatthews62@gmail.com.au>
2011-01-30 11:54:37 +1100

    Different test script for ScriptForAndroidTemplate Updated Help

--- android/ScriptForAndroidTemplate/res/raw/script.py
+++ android/ScriptForAndroidTemplate/res/raw/script.py
@@ -1,8 +1,17 @@
-import android
-print "Started"
+import android,time
 droid = android.Android()
 droid.makeToast('Hello, Android!')
 droid.vibrate(300)
-sleep(5000)
-print "Done"
+
+try:
+  droid.startSensing()
+  time.sleep(1)
+  e=droid.eventPoll(1)
+  droid.eventClearBuffer()
+  droid.makeToast("Polled: "+str(e))
+except:
+  droid.makeToast("Unexpected error:"+str(sys.exc_info()[0]))
+
+droid.makeToast("Done")  
+
 
